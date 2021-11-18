c6eb000441061ead1535073f71443912dfab9478
khosyrobbin <khosybin@gmail.com>
2021-11-18 16:17:15 +0700

    Update detail penduduk

--- resources/views/layout/detailPenduduk.blade.php
+++ resources/views/layout/detailPenduduk.blade.php
@@ -10,43 +10,44 @@
                             <h4 class="card-title">Data Penduduk</h4>
                         </div>
                         <div class="card-body">
-                            <table class="table">
-                                <tr>
-                                    <th width="100px">NIK</th>
-                                    <th width="30px">:</th>
-                                    <th>{{$penduduk->nik}}</th>
-                                </tr>
-                                <tr>
-                                    <th width="100px">Nama</th>
-                                    <th width="30px">:</th>
-                                    <th>{{$penduduk->name}}</th>
-                                </tr>
-                                <tr>
-                                    <th width="100px">Jenis Kelamin</th>
-                                    <th width="30px">:</th>
-                                    <th>{{$penduduk->jk}}</th>
-                                </tr>
-                                <tr>
-                                    <th width="100px">No Telepon</th>
-                                    <th width="30px">:</th>
-                                    <th>{{$penduduk->no_telpon}}</th>
-                                </tr>
-                                <tr>
-                                    <th width="100px">Alamat</th>
-                                    <th width="30px">:</th>
-                                    <th>{{$penduduk->alamat}}</th>
-                                </tr>
-                                <tr>
-                                    <th width="100px">Foto KTP</th>
-                                    <th width="30px">:</th>
-                                    <th><img src="{{url('foto_ktp/'.$penduduk->foto)}}" width="150px"></th>
-                                </tr>
-                                <th>
-                                    <div class="form-group">
-                                        <a href="/penduduk" class="btn btn-success btn-sm">Kembali</a>
-                                    </div>
-                                </th>
-                            </table>
+                            <form action="/penduduk">
+                                @csrf
+                                <table class="table">
+                                    <tr>
+                                        <th width="100px">NIK</th>
+                                        <th width="30px">:</th>
+                                        <th>{{$penduduk->nik}}</th>
+                                    </tr>
+                                    <tr>
+                                        <th width="100px">Nama</th>
+                                        <th width="30px">:</th>
+                                        <th>{{$penduduk->name}}</th>
+                                    </tr>
+                                    <tr>
+                                        <th width="100px">Jenis Kelamin</th>
+                                        <th width="30px">:</th>
+                                        <th>{{$penduduk->jk}}</th>
+                                    </tr>
+                                    <tr>
+                                        <th width="100px">No Telepon</th>
+                                        <th width="30px">:</th>
+                                        <th>{{$penduduk->no_telpon}}</th>
+                                    </tr>
+                                    <tr>
+                                        <th width="100px">Alamat</th>
+                                        <th width="30px">:</th>
+                                        <th>{{$penduduk->alamat}}</th>
+                                    </tr>
+                                    <tr>
+                                        <th width="100px">Foto KTP</th>
+                                        <th width="30px">:</th>
+                                        <th><img src="{{url('foto_ktp/'.$penduduk->foto)}}" width="150px"></th>
+                                    </tr>
+                                </table>
+                                <div class="form-group">
+                                    <button class="btn btn-primary pull-right">Kembali</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
