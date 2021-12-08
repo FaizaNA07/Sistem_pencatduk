4a20c1717847d2a38333cb28781e804869d6b168
khosyrobbin <khosybin@gmail.com>
2021-12-08 21:48:34 +0700

    update layout surat

--- resources/views/layout/surat.blade.php
+++ resources/views/layout/surat.blade.php
@@ -8,7 +8,7 @@
             <h4 class="card-title ">Surat Keterangan Tinggal Sementara(SKTS)</h4>
         </div>
         <div class="card-body">
-            <h6>jkdbakjbcekfugabcka</h6>
+            <h6>SKTS adalah surat keterangan yang diterbitkan untuk Warga Negara Indonesia sebagai Pendatang Sementara yang berniat menetap di daerah lain dalam wilayah RI selama 90 (Sembilan puluh) hari berturut turut sampai dengan 1 (satu) tahun.</h6>
         </div>
         <a href="/surat/cetakSKTS" target="_blank" class="d-flex justify-content-center">
             <button type="button" class="btn btn-success btn-round"><i class="material-icons">printer</i>Cetak SKTS</button>
@@ -19,7 +19,7 @@
             <h4 class="card-title ">Surat Keterangan Domisili Sementara (SKDS)</h4>
         </div>
         <div class="card-body">
-            <h6>jkdbakjbcekfugabcka</h6>
+            <h6>Surat Keterangan Domisili Sementara yang selanjutnya disingkat SKDS adalah identitas resmi sebagai bukti diri penduduk non permanen di Daerah.</h6>
         </div>
         <a href="/surat/cetakSKDS" target="_blank" class="d-flex justify-content-center">
             <button type="button" class="btn btn-success btn-round"><i class="material-icons">printer</i>Cetak SKDS</button>
